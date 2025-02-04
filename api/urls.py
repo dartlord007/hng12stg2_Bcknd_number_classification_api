23c8827e930b3c6b144856aa465c801d35190dcb
Kolade <kolademodupe007@gmail.com>
2025-02-04 20:27:36 +0100

    Remove home endpoint from URL patterns and retain number classifier endpoint

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from .views import NumberClassifierView 
 
 urlpatterns = [
-    path('', NumberClassifierView.as_view(), name='home'),
     path('classify-number/', NumberClassifierView.as_view(), name='number-classifier'),
 ]
