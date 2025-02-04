21206cf519c9f5f36876b01b1f40c05c86793b22
Kolade <kolademodupe007@gmail.com>
2025-02-04 20:11:30 +0100

    Add import statement for views in URL configuration

--- number_classification_api/urls.py
+++ number_classification_api/urls.py
@@ -17,10 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from api import views
+
 # from api import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('api/', include('api.urls')),
-    # path('', views.home, name='home'),
 ]
