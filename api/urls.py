c14769f28a35bbe92c8436130945b7481fa5664f
Kolade <kolademodupe007@gmail.com>
2025-02-04 20:40:30 +0100

    Update number classifier endpoint to use root URL

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import NumberClassifierView 
 
 urlpatterns = [
-    path('classify-number/', NumberClassifierView.as_view(), name='number-classifier'),
+   
+ path('', NumberClassifierView.as_view(), name='number-classifier'),
 ]
