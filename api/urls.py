f71292d6ca8473d6ce1a494f54ace610d9c33ba6
Kolade <kolademodupe007@gmail.com>
2025-02-05 09:36:32 +0100

    Add static file serving in development mode to URL patterns

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
+from django.conf.urls.static import static
+
+from number_classification_api import settings
 from .views import number_classifier
 
 urlpatterns = [
     path('', number_classifier, name='number_classifier'),
     path('classify-number/', number_classifier, name='number_classifier'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
